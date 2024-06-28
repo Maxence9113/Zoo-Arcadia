a7be776edfd296103aa9c1a1b56181629f728e39
Maxence <dupuis.maxence9@gmail.com>
2024-06-28 10:25:47 +0000

    import information dans la vue des compte utilisateur

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class AccountController extends AbstractController
 {
     #[Route('/compte', name: 'app_account')]
-    public function index(): Response
+    public function index(#[CurrentUser] ?User $user): Response
     {
-        return $this->render('account/index.html.twig');
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+        return $this->render('account/index.html.twig', [
+            'user' => $user,]);
     }
 }
