fc4ffd8c98f92338d9543609a61e696671bf5feb
Maxence <dupuis.maxence9@gmail.com>
2024-06-29 20:15:01 +0000

    Modification du formulaire d'incription

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,8 +4,15 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class UserType extends AbstractType
 {
@@ -17,6 +24,56 @@ class UserType extends AbstractType
             ->add('email')
             // ->add('password')
             ->add('profil')
+
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Doe',
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'John',
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+                'attr' => [
+                    'placeholder' => 'Entrez votre adresse email',
+                ]
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options'  => [
+                    'label' => 'Entrez votre mot de passe', 'hash_property_path' => 'password',
+                    'attr' => [
+                        'placeholder' => 'Entrez votre mot de passe',
+                    ],
+                    'constraints' => [new Length(['min' => 3])],
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez votre mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Confirmez votre mot de passe',
+                    ],
+                ],
+                'mapped' => false,
+            ])
+            ->add(
+                'profil',
+                ChoiceType::class,
+                [
+                    'choices'  => [
+                        'Administrateur' => 'ROLE_ADMIN',
+                        'Vétérinaire' => 'ROLE_VETERINARY',
+                        'Employé' => 'ROLE_EMPLOYEE',
+                    ],
+                    'multiple' => false,
+                    'label' => 'Rôle',
+                ]
+            )
+
         ;
     }
 
