594fa05dd78c9cddff251d855b38480b1152751e
Maxence <dupuis.maxence9@gmail.com>
2024-07-03 20:29:22 +0000

    gestion des ajout multiple OK

--- src/Form/PictureAnimalType.php
+++ src/Form/PictureAnimalType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\All;
 use Symfony\Component\Validator\Constraints\File;
 
 class PictureAnimalType extends AbstractType
@@ -20,16 +21,20 @@ class PictureAnimalType extends AbstractType
             ->add('picture_animal', FileType::class, [
                 'label' => 'Image (jpeg, webp)',
                 'mapped' => false,
-                'multiple' => false,
+                'multiple' => true,
                 'required' => false,
                 'constraints' => [
-                    new File([
-                        // 'maxSize' => '1024k',
-                        'mimeTypes' => [
-                            'image/jpeg',
-                            'image/webp',
-                        ],
-                        'mimeTypesMessage' => 'Please upload a valid image file',
+                    new All([
+                        'constraints' => [
+                            new File([
+                                // 'maxSize' => '1024k',
+                                'mimeTypes' => [
+                                    'image/jpeg',
+                                    'image/webp',
+                                ],
+                                'mimeTypesMessage' => 'Please upload a valid image file',
+                            ])
+                        ]
                     ])
                 ],
             ])
